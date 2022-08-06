874fe946ceccc9ff347ca8abaafc77b7e70ae1bb
Borachok <bogeton.xd@gmail.com>
2022-08-06 18:32:58 +0300

    change icons to another

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="./src/icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./src/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./src/icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
