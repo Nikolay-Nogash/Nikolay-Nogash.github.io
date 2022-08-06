4a28e4c2bc40113a1fa944eed56059f4576cf000
Borachok <bogeton.xd@gmail.com>
2022-08-06 18:26:52 +0300

    test

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../../src/icons/left.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
