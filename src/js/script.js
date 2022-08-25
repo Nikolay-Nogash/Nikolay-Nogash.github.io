fb83b8bc2627cadf2c75910372a22018b0d96f62
Nikolay-Nogash <mikolanogash7@gmail.com>
2022-08-25 17:48:22 +0300

    add animations

--- src/js/script.js
+++ src/js/script.js
@@ -82,4 +82,23 @@ $(document).ready(function(){
       validateForms('#consultation form');
       validateForms('#order form');
       
+      $('input[name=phone]').mask("+7 (999) 999-9999");
+
+      //Smooth scroll and pageup
+
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+      });
+
+      $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false
+      });
+
+      new WOW().init();
   });
